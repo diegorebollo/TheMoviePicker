6faf8d0bd763760038b43e3c97cd4e3234709e2e
Diego <diego@drebollo.com>
2024-02-01 14:55:51 +0100

    ngx-videoplayer

--- frontend/the-movie-picker/src/app/components/videoplayer/videoplayer.component.ts
+++ frontend/the-movie-picker/src/app/components/videoplayer/videoplayer.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-
-import {VgCoreModule, VgApiService } from '@videogular/ngx-videogular/core';
-import {VgControlsModule} from '@videogular/ngx-videogular/controls';
-import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
-import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
+import { VgApiService, VgCoreModule } from '@videogular/ngx-videogular/core';
+import { VgControlsModule } from '@videogular/ngx-videogular/controls';
+import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
+import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
 
 @Component({
   selector: 'app-videoplayer',
@@ -11,20 +10,29 @@ import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
   imports: [VgCoreModule,
     VgControlsModule,
     VgOverlayPlayModule,
-    VgBufferingModule],
+    VgBufferingModule,],
   templateUrl: './videoplayer.component.html',
   styleUrl: './videoplayer.component.css'
 })
 export class VideoplayerComponent {
   movieData!: any;
   movieURL!: any;
+  vgHls!: string;
+  api:VgApiService = new VgApiService;
 
 
   ngOnInit(){
     this.movieData = history.state.movie;
     this.movieURL = this.movieData.movieUrl.split(',')[2].split("b'")[1].split("'")[0];
+    // this.movieURL = this.movieData.movieUrl.split(',')[0].split("'")[1];
     console.log(this.movieURL)
  };
+
+  onPlayerReady(){
+    console.log(this.api)   
+    console.log('ddd') 
+  }
+
   
 
 
