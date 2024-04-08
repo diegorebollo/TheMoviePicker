962389045848e8148cc0c80d28d6499e02cf6a50
Diego Rebollo <111189622+diegorebollo@users.noreply.github.com>
2024-04-08 02:59:42 +0200

    Update tmdbApi.js

--- backend/src/utils/tmdbApi.js
+++ backend/src/utils/tmdbApi.js
@@ -2,7 +2,7 @@ const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTE3ZTMzMmI5N2UyNjBhYWMxYjVhMTY3MDIxM2Q2YSIsInN1YiI6IjYzNzNkMTRiOGZkZGE5MDA3N2RhNzE5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8oY9DS06lwaWeqOrf9pscss9birQWQgh1fKc_B8M2zU'
+      Authorization: ''
     }
   };
   
